37cf911e6910d275a205c31460aeba66da86f382
David Waza <idemetoemediong2022@gmail.com>
2025-11-14 15:52:45 +0100

    fix: formatted documents

--- app/middleware.ts
+++ app/middleware.ts
@@ -9,7 +9,7 @@ export async function middleware(req: NextRequest) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  console.log(session)
+  console.log(session);
 
   if (!session) {
     return NextResponse.rewrite(new URL("/login", req.url));
