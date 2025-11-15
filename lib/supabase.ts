1470ff725e6582e046e02d30032413cde78c7e3b
Waza <idemetoemediong2022@gmail.com>
2025-11-15 17:00:16 +0100

    changed supabase admin

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,9 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClientComponentClient();
+export const supabaseAdmin = createClientComponentClient();
 
-export const supabaseAdmin = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
